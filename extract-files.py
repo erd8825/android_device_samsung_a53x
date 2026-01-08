cd642538755f95a282235eafd515cf94dab02552
Majaahh <maja.blaszczak@proton.me>
2026-01-08 15:03:20 +0100

    a53x: Setup proprietary files
    Change-Id: I54074475be11eb25a6fde82423e54e98ad1a4b95
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ extract-files.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+module = ExtractUtilsModule(
+    'a53x',
+    'samsung',
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 's5e8825-common', module.vendor
+    )
+    utils.run()
