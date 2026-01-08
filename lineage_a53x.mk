1b4725b920764848a24027c1f1f6fe5e705965de
Majaahh <maja.blaszczak@proton.me>
2026-01-08 17:49:50 +0100

    a53x: Setup GMS
    Change-Id: I2e4033108e0b04990aaabdfe8a20690df924726c
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- lineage_a53x.mk
+++ lineage_a53x.mk
@@ -10,3 +10,9 @@ $(call inherit-product, device/samsung/a53x/device.mk)
 PRODUCT_DEVICE := a53x
 PRODUCT_NAME := lineage_a53x
 PRODUCT_MODEL := SM-A536B
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a53xnaxx-user 16 BP2A.250605.031.A3 A536BXXSOGZH2 release-keys" \
+    BuildFingerprint=samsung/a53xnaxx/essi:16/BP2A.250605.031.A3/A536BXXSOGZH2:user/release-keys \
+    DeviceProduct=a53xnaxx \
+    SystemName=a53xnaxx
