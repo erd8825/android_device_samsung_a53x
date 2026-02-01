9d3ecdfdd6f3f902bd155de869edf01eddba4d7f
Majaahh <maja.blaszczak@proton.me>
2026-02-01 20:46:12 +0100

    a53x: Setup Audio
    * mixer_gains.xml was merged in to mixer_paths.xml
    
    Change-Id: I52f58fe0eda789b7767e65a30263f3606ee86064
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, device/samsung/s5e8825-common/common.mk)
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/a53x/a53x-vendor.mk)
 
+# Audio - Configuration
+PRODUCT_PACKAGES += mixer_paths.xml
+
 # CAS
 TARGET_REQUIRES_HIDL_CAS_HAL := false
 
