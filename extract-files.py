63f41c11f46228db5bd220619b75ec8618ee6e63
Majaahh <maja.blaszczak@proton.me>
2026-02-21 17:51:47 +0100

    a53x: Setup Camera
    Change-Id: Ie807497f94528567da6b693b996e4c4edc82798f
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/samsung/s5e8825-common',
+    'hardware/samsung_slsi-linaro/exynos',
+    'hardware/samsung_slsi-linaro/graphics',
+    'vendor/samsung/s5e8825-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    # Camera
+    'vendor/lib64/libexynoscamera3.so': blob_fixup()
+        .add_needed('libshim_camera.so')
+        .binary_regex_replace(b'_ZN7android5Fence', b'_ZN7exynos55Fence'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'a53x',
     'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
 )
 
 module.add_proprietary_file('../s5e8825-common/proprietary-files.device.txt')
