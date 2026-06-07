2435529ffa7daf30e90989cec051131fea18168a
Majaahh <maja.blaszczak@proton.me>
2026-06-07 11:23:52 +0200

    a53x: Setup SEPolicy
    Change-Id: I26bb10b13a1c9553fcad9503df67127baa2126bb
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,8 @@
 # Inherit common board configuration
 include device/samsung/s5e8825-common/BoardConfigCommon.mk
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += device/samsung/a53x/sepolicy/vendor
+
 # Security
 VENDOR_SECURITY_PATCH := 2026-08-05
