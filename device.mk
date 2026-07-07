c9205f2414bc70a880bf284cdee504b525035cea
Majaahh <maja.blaszczak@proton.me>
2026-07-07 22:42:53 +0200

    a53x: Setup support for multiple device models
    * Some Audio Firmware and Firmware blobs are model-specific,
      so we need to mount them during boot for the model.
    * TEEgris Firmware must match user's tzsw version to even boot (full
      firmware string, not model), so we need to mount that during boot too.
      We will support only latest One UI 7.0 firmware (with exceptions if required).
    * To make sure that we don't mount the wrong blob, we use placeholders for
      those blobs. Thanks to them, we can make abox check if the firmware blob
      is a dummy. If it is a dummy, then it tries to load it later.
    * To avoid conflicts on devices without support for other variants,
      we have to keep the module definition for under soong config.
    
    Change-Id: Ib462e54e2f37be5b4705a9e31c066af4c2062c58
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@
 $(call inherit-product, device/samsung/s5e8825-common/products/aod/product.mk)
 $(call inherit-product, device/samsung/s5e8825-common/products/phone/product.mk)
 $(call inherit-product, device/samsung/s5e8825-common/products/sec_nfc/product.mk)
+$(call inherit-product, device/samsung/s5e8825-common/products/unify/product.mk)
 
 # Inherit the common tree
 $(call inherit-product, device/samsung/s5e8825-common/common.mk)
