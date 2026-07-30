51fc3048a30cb42651c6089efd517ceef51be02f
Majaahh <maja.blaszczak@proton.me>
2026-07-31 01:29:41 +0200

    a53x: shims: Minor adjustments
    Change-Id: I7c654daa0f4019390b7adb0c362b9e81bc21a074
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -24,7 +24,7 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     # Camera
     'vendor/lib64/libexynoscamera3.so': blob_fixup()
-        .add_needed('libshim_camera.so')
+        .add_needed('libcamera_shim.samsung.so')
         .add_needed('libui_shim.so')
         .binary_regex_replace(b'_ZN7android5Fence', b'_ZN7exynos55Fence')
         # Enable RAW support
