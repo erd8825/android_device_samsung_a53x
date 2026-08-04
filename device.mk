a024c890c41590cad0da248b23d3d4c48f3d00fd
Majaahh <maja.blaszczak@proton.me>
2026-08-04 19:54:32 +0200

    a53x: Stop building HIDL CAS
    Change-Id: If240e81a09a021aacd3cf5872af8ed343d2bdfc3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, device/samsung/s5e8825-common/common.mk)
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/a53x/a53x-vendor.mk)
 
+# CAS
+TARGET_REQUIRES_HIDL_CAS_HAL := false
+
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
