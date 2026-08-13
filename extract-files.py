f18d000acbfd56383e7eeea11e854f77ce21bee9
Majaahh <maja.blaszczak@proton.me>
2026-08-13 17:03:45 +0200

    a53x: Use libui from hardware/lineage/compat on libexynoscamera3
    Change-Id: I9a84bca6f342049fdec46ec43b41f0eebc4dff97
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,7 @@ blob_fixups: blob_fixups_user_type = {
     # Camera
     'vendor/lib64/libexynoscamera3.so': blob_fixup()
         .add_needed('libshim_camera.so')
+        .add_needed('libui_shim.so')
         .binary_regex_replace(b'_ZN7android5Fence', b'_ZN7exynos55Fence'),
 }  # fmt: skip
 
